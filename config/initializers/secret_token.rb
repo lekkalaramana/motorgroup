64ea188616bf072d317b2a5df39aa0197b273913
Ramana <ramana.lekkal@gmail.com>
2016-07-24 12:17:14 +0530

    motorgroup project first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Motorgroup::Application.config.secret_key_base = '8e4356f15f08bd38e0c5cfe643c536fe7a87eded0f4b90b72edea47638563fc93650a04211ea73122770cd4a54a91f493d0517a4b04f6216d1f85a57b235bd90'
