28a87b1a81e9f9d5498eaac67af90ae1bdf5d1e8
Ramana <ramana.lekkal@gmail.com>
2016-07-24 15:48:37 +0530

    changes in new page, vehical model, controller

--- app/controllers/vehicle_controller.rb
+++ app/controllers/vehicle_controller.rb
@@ -1,9 +1,9 @@
 class VehicleController < ApplicationController
   def show
+  	@vehical_details = Vehical.all
   end
 
   def create
-    
   end
 
   def new
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,2 +1,13 @@
 class Vehicle < ActiveRecord::Base
+	validates :model_name, presence: true
+	validates :model_code, uniqueness: true
+	validates :manufacturer, presence: true
+	validates :with_battery, presence: true
+	validates :with_tools, presence: true
+	validates :fuel_capacity, presence: true
+
+
+	def self.create_or_update params
+		Vehicle.create!(params)
+	end
 end
